f402783adf9142464ef980e213c66c17f3e74574
kunal-ch <kunalch36@gmail.com>
2022-09-22 00:07:30 +0530

    - Layout

--- app/src/main/java/com/kc/chatcomposeapp/MainActivity.kt
+++ app/src/main/java/com/kc/chatcomposeapp/MainActivity.kt
@@ -1,34 +1,56 @@
 package com.kc.chatcomposeapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kc.chatcomposeapp.ui.theme.ChatComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PreviewMessageCard()
+            MessageCard(message = Message("Steve", "5 am rule"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String){
-    Text(text = name)
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.sample_profile_pic),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(text = message.body)
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard(name = "Hello World")
+fun PreviewMessageCard() {
+    MessageCard(message = Message("Dr Kalam", "Wings of Fire"))
 }
 
